d302e978547463e0b018f9a24e0f7824accc8deb
McKinley-TheProProgrammer <McKinleyTheProProgrammer@users.noreply.github.com>
2020-09-05 19:30:38 -0300

    DEMO COMPLETED

---
+++ DesafioDeMerlinShader/Assets/Scripts/LoadAnotherScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAnotherScene : MonoBehaviour
+{
+    //[SerializeField] Scene cena;
+    public void GoToMenuScene()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void GoToScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
